19163e325313e1d08231a2acfa25995b4fc0648e
viktor <viktordojnov@hotmail.com>
2020-12-04 20:56:16 -0800

    computer transfer

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// FUNCTIONS
+const add = function(a,b) {
+    return a + b;
+}
+
+const subtract = function(a,b) {
+    return a - b;
+}
+
+const multiply = function(a,b) {
+    return a * b;
+}
+
+const divide = function(a,b) {
+    return a / b;
+}
+
+const operate = function(a,func,b) {
+    return func(a,b);
+}
+

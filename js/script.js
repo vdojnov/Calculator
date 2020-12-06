b8514138989476ace42d1f7f020b3452c02cb40f
Viktor Dojnov <dojnov.viktor@gmail.com>
2020-12-06 02:08:46 -0800

    styling changes and function implementations

--- js/script.js
+++ js/script.js
@@ -1,3 +1,14 @@
+// VARIABLES
+let firstNum = null;
+let secondNum = null;
+let operation = null;
+
+// TAGS
+const calcView = document.querySelector("#calc-view");
+const nums = document.querySelectorAll(".num")
+const funcs = document.querySelectorAll(".func")
+const view = document.querySelector('#calc-view')
+
 // FUNCTIONS
 const add = function(a,b) {
     return a + b;
@@ -19,3 +30,58 @@ const operate = function(a,func,b) {
     return func(a,b);
 }
 
+function updateDisplayString(letter) {
+    displayString += letter;
+    calcView.textContent = displayString;
+} 
+
+const addNumber = function() {
+    if (view.textContent.includes(".") & this.textContent==="."){
+        // DO NOTHING
+    } else{
+        if (view.textContent==="0"){
+            view.textContent = this.textContent;
+        } else {
+            view.textContent += this.textContent;
+        }        
+    }
+}
+
+const returnOperation = function(letter) {
+    switch(this.textContent) {
+        case "+":
+            return add;
+            break;
+        case "-":
+            return subtract;
+            break;
+        case "/":
+            return divide;
+            break;
+        case "*":
+            return multiply;
+            break;    
+      }
+}
+
+const handleFunc = function(letter) {
+   if (firstNum === null & operation === null) {
+        firstNum = +view.textContent
+        operation = returnOperation(letter)
+   }
+}
+
+
+// LOGIC
+funcs.forEach(element => {
+    element.addEventListener("click", handleFunc)    
+});
+
+nums.forEach(element => {
+    element.addEventListener("click", addNumber);
+})
+
+// function() {
+//     console.log(element.textContent)
+//     view.textContent += element.textContent
+// })    

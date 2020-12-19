08f86fd553be85a21aaa89547d2ccc604e5d6cd1
Viktor Dojnov <dojnov.viktor@gmail.com>
2020-12-18 21:39:11 -0800

    add button functionality

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ const calcView = document.querySelector("#calc-view");
 const nums = document.querySelectorAll(".num")
 const funcs = document.querySelectorAll(".func")
 const view = document.querySelector('#calc-view')
+const calcs = document.querySelectorAll(".calc")
 
 // FUNCTIONS
 const add = function(a,b) {
@@ -27,7 +28,12 @@ const divide = function(a,b) {
 }
 
 const operate = function(a,func,b) {
-    return func(a,b);
+    if (func===subtract & b === 0) {
+        return "yikes"
+    } else {
+        return func(a,b);
+    }
+    
 }
 
 function updateDisplayString(letter) {
@@ -47,41 +53,79 @@ const addNumber = function() {
     }
 }
 
-const returnOperation = function(letter) {
-    switch(this.textContent) {
-        case "+":
-            return add;
-            break;
-        case "-":
-            return subtract;
-            break;
-        case "/":
-            return divide;
-            break;
-        case "*":
-            return multiply;
-            break;    
-      }
+const returnOperation = function(sign) {
+    console.log(sign==="+")
+    // SWITCH STATEMENT NOT WORKING HERE??? WHY?
+    if (sign==="+"){
+        return add;
+    }
+    if (sign==="-"){
+        return subtract;
+    }
+    if (sign==="/"){
+        return divide;
+    }
+    if (sign==="x"){
+        return multiply;
+    }
 }
 
-const handleFunc = function(letter) {
-   if (firstNum === null & operation === null) {
-        firstNum = +view.textContent
-        operation = returnOperation(letter)
+
+const handleCalc = function() {
+    console.log("calcs")
+    if (firstNum === null & operation === null) {
+        firstNum = +view.textContent;
+        operation = returnOperation(this.textContent);
+        // console.log(operation);
+        view.textContent = "";
    }
 }
 
+const handleFuncs = function() {
+    console.log("funcs")
+    
+    switch(this.textContent) {
+        case "<":
+            view.textContent = view.textContent.slice(0, view.textContent.length - 1)
+            break;
+        case "Clear":
+            firstNum = null;
+            secondNum = null;    
+            operation = null;
+            view.textContent = 0;
+            break;
+        case "+/-":
+            if (view.textContent[0] === "-") {
+                view.textContent = view.textContent.slice(1, view.textContent.length);
+            } else {
+                view.textContent = "-" + view.textContent;
+            }
+            break;
+        case "=":
+            secondNum = +view.textContent 
+            view.textContent = operate(firstNum, operation, secondNum);
+            firstNum = null;
+            secondNum = null;    
+            operation = null;
+            break;
+      }
+}
+
+
+
+
 
 // LOGIC
-funcs.forEach(element => {
-    element.addEventListener("click", handleFunc)    
+calcs.forEach(element => {
+    element.addEventListener("click", handleCalc)    
 });
 
 nums.forEach(element => {
     element.addEventListener("click", addNumber);
 })
 
-// function() {
-//     console.log(element.textContent)
-//     view.textContent += element.textContent
-// })    
+funcs.forEach(element => {
+    element.addEventListener("click", handleFuncs);
+})
+
+
